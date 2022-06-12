03e4fdc6340554c6cfe9b85c34c08611bdfbc277
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-11 22:51:39 -0700

    Create renderless component for to useHead plugin globally

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <animated-router-view />
+  <use-head-globally>
+    <animated-router-view />
+  </use-head-globally>
 </template>
 
 <script lang="ts" setup>
 import AnimatedRouterView from '@/components/AnimatedRouterView.vue'
-// define global usage useHead for every route based on route.name
-import { useRoute } from "vue-router";
-import { useHead } from "@vueuse/head";
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
-
-const { t } = useI18n()
-const route = useRoute()
-const formatTitle = (name: string) => {
-  // do 1 letter uppercase
-  return (name.charAt(0).toUpperCase() + name.slice(1))
-    .replace("-", " ")
-    .replace("_", " ")
-}
-
-const currentRouteName = computed(() => route.name !== undefined ? formatTitle(String(route.name)) : t('messages.dashboard'))
-
-useHead({
-  title: currentRouteName,
-})
-// end block
+import UseHeadGlobally from '@/components/UseHeadGlobally.vue'
 </script>
