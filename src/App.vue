a258c450f1a99e5ff2ff81d926dac96f7f1768f8
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-11 22:42:08 -0700

    implement plugin useHead globally

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,25 @@
 
 <script lang="ts" setup>
 import AnimatedRouterView from '@/components/AnimatedRouterView.vue'
+// define global usage useHead for every route based on route.name
+import { useRoute } from "vue-router";
+import { useHead } from "@vueuse/head";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n()
+const route = useRoute()
+const formatTitle = (name: string) => {
+  // do 1 letter uppercase
+  return (name.charAt(0).toUpperCase() + name.slice(1))
+    .replace("-", " ")
+    .replace("_", " ")
+}
+
+const currentRouteName = computed(() => route.name !== undefined ? formatTitle(String(route.name)) : t('messages.dashboard'))
+
+useHead({
+  title: currentRouteName,
+})
+// end block
 </script>
