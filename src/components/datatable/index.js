76e7e00015a411f19227ec4f98a56625cb2d780b
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-19 21:56:12 -0700

    export datatable component via 1 export file

---
+++ src/components/datatable/index.js
@@ -0,0 +1,5 @@
+import ActionCreate from './actions/Create.vue'
+import ActionDelete from './actions/Delete.vue'
+import ActionFilter from './actions/Filter.vue'
+
+export { ActionCreate, ActionDelete, ActionFilter }
