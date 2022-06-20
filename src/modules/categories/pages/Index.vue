4a75ca1571e38831ee47bcee010a4efaeb57c4c2
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-19 23:18:59 -0700

    init first table with data

---
+++ src/modules/categories/pages/Index.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card :title="t('messages.categories')" elevation="4">
+
+          <div class="table-toolbar">
+            <action-create v-if="can('upsert', 'category')" :to="{ name: 'category-create' }" />
+          </div>
+
+          <v-table>
+            <thead>
+            <tr>
+              <th v-for="header in headers" :key="header.title" class="text-left">
+                {{ header.text }}
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+              v-for="category in categories.data"
+              :key="category.id"
+            >
+              <td>{{ category.id }}</td>
+              <td>{{ category.title }}</td>
+              <td>{{ dayjs(category.created_at).format('YYYY-MM-DD HH:mm') }}</td>
+              <td>{{ dayjs(category.updated_at).format('YYYY-MM-DD HH:mm') }}</td>
+            </tr>
+            </tbody>
+          </v-table>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn size="small" color="primary" @click="prev">
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+            <v-btn size="small" color="primary" @click="next">
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n"
+import { useQuery } from '@vue/apollo-composable'
+import { CategoriesAll } from '@/modules/categories/graphql/queries/category.gql'
+import { computed } from "vue"
+import { useAbility } from "@casl/vue"
+import pagination from "@/composables/usePagination"
+import { ActionCreate } from "@/components/datatable/index"
+import dayjs from 'dayjs'
+
+const { can } = useAbility()
+const { t } = useI18n()
+
+const headers = [
+  { text: '#', value: 'id' },
+  { text: t('messages.title'), value: 'title' },
+  { text: t('messages.updated_at'), value: 'updated_at' },
+  { text: t('messages.created_at'), value: 'created_at' }
+]
+
+// tmp functions
+const next = () => {
+  pagination.page++
+}
+const prev = () => {
+  pagination.page--
+}
+
+const { result } = useQuery(CategoriesAll, { pagination }, { clientId: 'public' })
+const categories = computed(() => result.value?.categories ?? { data: [] })
+</script>
