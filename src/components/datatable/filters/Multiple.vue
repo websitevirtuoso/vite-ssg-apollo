27802b3636c5bd8aff41dd0be4014faa60d46333
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-20 12:56:07 -0700

    add datatable filter to select multiple values

---
+++ src/components/datatable/filters/Multiple.vue
@@ -0,0 +1,3 @@
+<template>
+  <v-combobox v-bind="$attrs" multiple chips closable-chips clearable />
+</template>
--- src/components/datatable/index.js
+++ src/components/datatable/index.js
@@ -2,5 +2,6 @@ import ActionCreate from './actions/Create.vue'
 import ActionDelete from './actions/Delete.vue'
 import ActionFilter from './actions/Filter.vue'
 import FilterByText from './filters/Text.vue'
+import FilterByMultiple from './filters/Multiple.vue'
 
-export { ActionCreate, ActionDelete, ActionFilter, FilterByText }
+export { ActionCreate, ActionDelete, ActionFilter, FilterByText, FilterByMultiple }
