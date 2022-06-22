fa95460d566802ce157026afee4f4919e385bf79
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-22 10:45:16 -0700

    add extra validation param

--- src/modules/regions/helpers/validation_schema_state.ts
+++ src/modules/regions/helpers/validation_schema_state.ts
@@ -4,7 +4,7 @@ import { tDefinition } from '@/types/i18n'
 export default (t: tDefinition) => {
   return object({
     name: string().required().label(t('messages.name')),
-    code: string().required().label(t('messages.code')),
+    code: string().required().max(10).label(t('messages.code')),
     country_id: string().required().label(t('messages.country')),
   })
 }
