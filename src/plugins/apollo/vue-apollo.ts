c38c5744376629e3887b702ff2928ca35dfef4db
Viktor Denisov <websitevirtuoso@gmail.com>
2022-06-23 15:45:15 -0700

    add gql operation name into requests for cypress tests

--- src/plugins/apollo/vue-apollo.ts
+++ src/plugins/apollo/vue-apollo.ts
@@ -9,13 +9,20 @@ import router from '@/plugins/router'
 const httpEndpoint = import.meta.env.VITE_GRAPHQL_HTTP
 const httpPublicEndpoint = httpEndpoint + '/public'
 
-const httpLink = createHttpLink({
-  uri: httpEndpoint,
-})
+// for cypress need to extend gql request to identify each request
+// https://github.com/cypress-io/cypress-documentation/issues/122#issuecomment-634693934
+const gqlCypressAddOperationNames = (input: RequestInfo | URL, options: RequestInit) => {
+  // @ts-expect-error we know that options.body defined
+  return fetch(`${input}?operation=${JSON.parse(options.body).operationName}`, options)
+}
 
-const httpPublicLink = createHttpLink({
-  uri: httpPublicEndpoint,
-})
+const prepareHttpLinkOptions = (uri: string) => {
+  return import.meta.env.MODE !== 'production' ? { uri, fetch: gqlCypressAddOperationNames } : { uri }
+}
+
+const httpLink = createHttpLink(prepareHttpLinkOptions(httpEndpoint))
+
+const httpPublicLink = createHttpLink(prepareHttpLinkOptions(httpPublicEndpoint))
 
 // Handle errors globally
 // https://v4.apollo.vuejs.org/guide-composable/error-handling.html#network-errors
@@ -26,6 +33,7 @@ const errorLink = onError((error) => {
   }
   // @ts-expect-error invalid definition in TS for this use case
   if (networkError && networkError.statusCode === 401) {
+    // todo call notification plugin and add message - 'Please authenticate'
     router.push({ name: 'login' })
   }
 })
