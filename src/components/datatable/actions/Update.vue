c43676c4c483a146672c7eb38c2402c6c8997fb2
Viktor Denisov <websitevirtuoso@gmail.com>
2022-07-07 22:24:50 -0700

    Added component for datatable edit action

---
+++ src/components/datatable/actions/Update.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip :text="text" location="top">
+    <template #activator="{ props: lprops }">
+      <v-btn
+        v-bind="lprops" icon="mdi-pencil" size="small" flat
+        test-attr="update" :text="props.text" class="mr-1" @click="$emit('click')"/>
+    </template>
+  </v-tooltip>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
--- src/components/datatable/index.js
+++ src/components/datatable/index.js
@@ -1,7 +1,8 @@
 import ActionCreate from './actions/Create.vue'
+import ActionUpdate from './actions/Update.vue'
 import ActionDelete from './actions/Delete.vue'
 import ActionFilter from './actions/Filter.vue'
 import FilterByText from './filters/Text.vue'
 import FilterByMultiple from './filters/Multiple.vue'
 
-export { ActionCreate, ActionDelete, ActionFilter, FilterByText, FilterByMultiple }
+export { ActionCreate, ActionUpdate, ActionDelete, ActionFilter, FilterByText, FilterByMultiple }
