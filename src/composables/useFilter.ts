7a8deb0c43b40a1668c95e0b80fe8efb742efa8a
Viktor Denisov <websitevirtuoso@gmail.com>
2022-07-17 21:36:39 -0700

    improve function for filtering in gql queries

--- src/composables/useFilter.ts
+++ src/composables/useFilter.ts
@@ -2,9 +2,21 @@ import { computed, reactive } from 'vue'
 
 const rawFilter = reactive({})
 
+export const excludeEmptyValues = (obj: object) => {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, v]) => {
+      // if empty array exclude it
+      if (Array.isArray(v) && v.length === 0) {
+        return false
+      }
+      return v !== null && v !== '' && v !== undefined
+    })
+  )
+}
+
 const filter = computed(() => {
   // find empty values and remove with keys
-  return Object.fromEntries(Object.entries(rawFilter).filter(([_, v]) => v !== null && v !== ''))
+  return excludeEmptyValues(rawFilter)
 })
 
 export { rawFilter, filter }
