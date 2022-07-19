be52d17561da847418633d4770954baa2fe89cc9
Viktor Denisov <websitevirtuoso@gmail.com>
2022-07-18 21:33:48 -0700

    set data-test attr for testing

--- src/modules/regions/components/CityAliasFilter.vue
+++ src/modules/regions/components/CityAliasFilter.vue
@@ -3,10 +3,12 @@
     <v-card-text>
       <v-row>
         <v-col cols="12" md="6">
-          <filter-by-multiple v-model="rawFilter.id" type="number" :hint="t('messages.multiple_values')" :label="t('messages.id', 2)" />
+          <filter-by-multiple
+            v-model="rawFilter.id" type="number" :hint="t('messages.multiple_values')"
+            :label="t('messages.id', 2)" data-test="filter.alias.id" />
         </v-col>
         <v-col cols="12" md="6">
-          <filter-by-text v-model="rawFilter.name" :label="t('messages.name')" />
+          <filter-by-text v-model="rawFilter.name" :label="t('messages.name')" data-test="filter.alias.name" />
         </v-col>
       </v-row>
     </v-card-text>
--- src/modules/regions/components/CityFilter.vue
+++ src/modules/regions/components/CityFilter.vue
@@ -3,23 +3,25 @@
     <v-card-text>
       <v-row>
         <v-col cols="12" md="3">
-          <filter-by-multiple v-model="rawFilter.id" type="number" :hint="t('messages.multiple_values')" :label="t('messages.id', 2)" />
+          <filter-by-multiple v-model="rawFilter.id" type="number" :hint="t('messages.multiple_values')" :label="t('messages.id', 2)" data-test="filter.city.id" />
         </v-col>
         <v-col cols="12" md="3">
-          <filter-by-text v-model="rawFilter.name" :label="t('messages.name')" />
+          <filter-by-text v-model="rawFilter.name" :label="t('messages.name')" data-test="filter.city.name" />
         </v-col>
         <v-col cols="12" md="3">
           <states-query v-slot="{ items, loading }">
             <v-combobox
-              v-model="rawFilter.state_id" :items="items" :label="t('messages.state')"
-              item-title="name" item-value="id" multiple chips closable-chips clearable :loading="loading" hide-details :return-object="false" />
+              v-model="rawFilter.state_id" :items="items" :label="t('messages.state')" :loading="loading"
+              :return-object="false" multiple chips closable-chips clearable
+              hide-details item-title="name" item-value="id" data-test="filter.city.state" />
           </states-query>
         </v-col>
         <v-col cols="12" md="3">
           <countries-query v-slot="{ items, loading }">
             <v-select
               v-model="rawFilter.country_id" :items="items" :label="t('messages.country')"
-              item-title="name" item-value="id" :loading="loading" hide-details clearable
+              :loading="loading" hide-details clearable item-title="name" item-value="id"
+              data-test="filter.city.country"
             />
           </countries-query>
         </v-col>
