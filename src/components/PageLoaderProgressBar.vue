659f735224f042595ed4e35b909ea26b7a366565
Viktor Denisov <websitevirtuoso@gmail.com>
2022-08-28 23:10:15 -0700

    Add page loader

---
+++ src/components/PageLoaderProgressBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-progress-linear
+    :active="loading"
+    :indeterminate="loading"
+    absolute
+    bottom
+    color="primary"
+  />
+</template>
+
+<script setup lang="ts">
+import { useGlobalQueryLoading, useGlobalMutationLoading } from '@vue/apollo-composable'
+
+const loading = useGlobalQueryLoading() || useGlobalMutationLoading()
+</script>
--- src/components/layouts/Authorized.vue
+++ src/components/layouts/Authorized.vue
@@ -3,6 +3,7 @@
     <SideBar />
     <AppBar />
     <v-main>
+      <page-loader-progress-bar />
       <v-container fluid class="pa-0">
         <v-row no-gutters>
           <v-col cols="12">
@@ -20,4 +21,5 @@ import AppBar from '@/components/navigation/AppBar.vue'
 import SideBar from '@/components/navigation/SideBar.vue'
 import Breadcrumbs from '@/components/Breadcrumbs.vue'
 import AnimatedRouterView from '@/components/AnimatedRouterView.vue'
+import PageLoaderProgressBar from '@/components/PageLoaderProgressBar.vue'
 </script>
