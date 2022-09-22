a0e0512bf81d488cd04e04ca9f73af0b42e0d1be
Viktor Denisov <websitevirtuoso@gmail.com>
2022-09-21 23:39:35 -0700

    Add v-error-message

---
+++ src/components/fields/VErrorMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <ErrorMessage v-slot="{ message }" :name="name">
+    <div class="v-input__details">
+      <div class="v-messages validation__error">
+        <div class="v-messages__message">
+          {{ message }}
+        </div>
+      </div>
+    </div>
+  </ErrorMessage>
+</template>
+
+<script setup lang="ts">
+import { ErrorMessage } from 'vee-validate'
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
