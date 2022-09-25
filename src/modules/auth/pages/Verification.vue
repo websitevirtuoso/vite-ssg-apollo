1eac3c33925bf1d156dbce2aee641a0ad280fbd9
Viktor Denisov <websitevirtuoso@gmail.com>
2022-09-24 23:48:19 -0700

    Add verification page

---
+++ src/modules/auth/pages/Verification.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="8">
+        <v-card title="Verify Your Email Address">
+          <v-card-text>
+            <v-alert border="start" color="indigo" class="mb-4"> A fresh verification link has been sent to your email address.</v-alert>
+            Before proceeding, please check your email for a verification link.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
